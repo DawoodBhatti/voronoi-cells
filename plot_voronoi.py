c5c94c10b564294b11cdd185b54f77f6b3aedc6a
Ali B <d.a.bhatti@hotmail.com>
2025-05-08 12:59:37 +0100

    experimental changes

--- plot_voronoi.py
+++ plot_voronoi.py
@@ -1,42 +1,24 @@
 import queue
 import threading
 import turtle
-import math
-import random
 from voronoi_cells import generate_voronoi_cells
-
-#TODO: add voronoi levels to this, e.g. remove some points and replot with different filters, colours, etc
-#TODO: mess around with fitting ellipsoids to shapes?
-#TODO: try creating a shading profile
-#TODO: translate to gdscript
-
-# Global variables for screen size
-turtle.TurtleScreen._RUNNING = True
-
-WINDOW_WIDTH = turtle.window_width()
-WINDOW_HEIGHT = turtle.window_height()
-
-WIDTH = 0.9
-HEIGHT = 0.9
-
-X_RANGE = WINDOW_WIDTH * WIDTH  
-Y_RANGE = WINDOW_HEIGHT * HEIGHT  
+from voronoi_to_delauney import generate_quadrants
 
 class TurtleDrawing:
-    def __init__(self, name, vertices):
-        """Immediately draw a Voronoi cell upon turtle creation."""
+    def __init__(self, name, vertices, spawn_draw=False):
+        """Create a turtle object to draw given vertices."""
         self.vertices = vertices
         self.turtle_object = turtle.Turtle()
         self.turtle_object.hideturtle()  # Keep turtle hidden
-
         self.turtle_object.speed("fastest")
         self.turtle_object.width(2)
         self.turtle_object.color("black")
-
-        self.draw_cell()
+        
+        if spawn_draw:        
+            self.draw_cell()
 
     def draw_cell(self):
-        """Draw the entire Voronoi cell instantly upon initialization."""
+        """Draw the cell defined by vertices."""
         self.turtle_object.penup()
         self.turtle_object.goto(self.vertices[0])
         self.turtle_object.pendown()
@@ -46,60 +28,123 @@ class TurtleDrawing:
 
         self.turtle_object.goto(self.vertices[0])  # Close the shape
 
-def draw_shapes(shape_vertices):
-    """Spawn and instantly draw each Voronoi cell."""
+def draw_voronoi_cells(shape_vertices):
+    """Queue Voronoi cell drawing operations."""
     for i, vertices in enumerate(shape_vertices):
-        graphics.put(lambda v=vertices: TurtleDrawing("turt" + str(i), v))
+        graphics.put(lambda v=vertices: TurtleDrawing("voronoi_" + str(i), v, True))
+
+def draw_seed_points(seed_points):
+    """Queue seed points for threaded drawing."""
+    for x, y in seed_points:
+        graphics.put(lambda x=x, y=y: draw_single_seed_point(x, y))
+
+def draw_single_seed_point(x, y):
+    """Draw an individual seed point."""
+    seed_turtle = turtle.Turtle()
+    seed_turtle.hideturtle()
+    seed_turtle.speed("fastest")
+    seed_turtle.color("blue")
+    
+    seed_turtle.penup()
+    seed_turtle.goto(x, y)
+    seed_turtle.pendown()
+    seed_turtle.dot(5)
+
+def draw_quadrants(quadrants):
+    """Queue quadrant drawing operations."""
+    for i, vertices in enumerate(quadrants):
+        graphics.put(lambda v=vertices: TurtleDrawing("quadrant_" + str(i), v, True))
 
 def process_queue():
-    """Process the function queue for execution."""
+    """Process queued drawing functions."""
     while not graphics.empty():
         (graphics.get())()
 
     turtle.ontimer(process_queue, 100)
 
-def plot_voronoi(shape_vertices, seed_points=None, show_seed_points=True, show_voronoi=True):
-    """Unified function to visualize seed points and Voronoi cells."""
+def plot_voronoi(shape_vertices):
+    """Start Voronoi cell plotting."""
+    global graphics
+    graphics = queue.Queue()
+
+    thread = threading.Thread(target=draw_voronoi_cells, args=(shape_vertices,))
+    thread.daemon = True
+    thread.start()
+
+def plot_seed_points(seed_points):
+    """Queue seed points for threaded drawing."""
+    for x, y in seed_points:
+        graphics.put(lambda x=x, y=y: draw_single_seed_point(x, y))
+
+    process_queue()
+
+def plot_quadrants(quadrants):
+    """Start quadrant plotting."""
+    global graphics
+    graphics = queue.Queue()
+
+    thread = threading.Thread(target=draw_quadrants, args=(quadrants,))
+    thread.daemon = True
+    thread.start()
+
+def initialize_turtle_screen():
+    """Initialize the Turtle screen settings."""
     turtle.TurtleScreen._RUNNING = True
     turtle.resetscreen()
     turtle.setup(width=0.9, height=0.9)
+    turtle.hideturtle()  # Hide the default turtle
+    
+def initialize_graphics_queue():
+    """Initialize the global graphics queue once."""
+    global graphics
+    graphics = queue.Queue()
 
-    if show_seed_points and seed_points:
-        seed_turtle = turtle.Turtle()
-        seed_turtle.hideturtle()
-        seed_turtle.speed("fastest")
-        seed_turtle.color("blue")
+def compute_window_size():
+    """Compute and set global window size variables."""
+    global WINDOW_WIDTH, WINDOW_HEIGHT, WIDTH, HEIGHT, X_RANGE, Y_RANGE
+    WINDOW_WIDTH = turtle.window_width()
+    WINDOW_HEIGHT = turtle.window_height()
+    WIDTH, HEIGHT = 0.9, 0.9
+    X_RANGE, Y_RANGE = WINDOW_WIDTH * WIDTH, WINDOW_HEIGHT * HEIGHT
 
-        for x, y in seed_points:
-            seed_turtle.penup()
-            seed_turtle.goto(x, y)
-            seed_turtle.pendown()
-            seed_turtle.dot(5)
+def generate_data():
+    """Generate Voronoi cells, seed points, and quadrants."""
+    try:
+        voronoi_data, seed_points = generate_voronoi_cells(
+            x_range=X_RANGE, y_range=Y_RANGE, num_points=15, 
+            offset_x=-X_RANGE/2, offset_y=-Y_RANGE/2, distribution_method="fibonacci"
+        )
+        quadrants = generate_quadrants(voronoi_data)
+        return voronoi_data, seed_points, quadrants
+    except Exception as e:
+        print(f"Error generating data: {e}")
+        return [], [], []
 
-    if show_voronoi:
-        global graphics
-        graphics = queue.Queue(3)
+def plot_graphics(voronoi_data, seed_points, quadrants):
+    """Plot seed points, Voronoi cells, and quadrants."""
+    plot_seed_points(seed_points)
+    plot_voronoi(voronoi_data)
+    # plot_quadrants(quadrants)
 
-        thread1 = threading.Thread(target=draw_shapes, args=(shape_vertices,))
-        thread1.daemon = True
-        thread1.start()
+def main():
+    """Main function to initialize everything and start plotting."""
+    initialize_turtle_screen()
+    initialize_graphics_queue()
+    compute_window_size()
 
-        process_queue()
+    # Generate Data
+    voronoi_data, seed_points, quadrants = generate_data()
 
+    # Start plots
+    plot_graphics(voronoi_data, seed_points, quadrants)
+
+    # Start queue processing
+    process_queue()
+
+    # Keep the turtle window open
     turtle.done()
     print("Plotting complete!")
 
-# Generate Voronoi cells and plot
-voronoi_data, seed_points = generate_voronoi_cells(x_range=X_RANGE, y_range=Y_RANGE, 
-                                                   num_points=120, 
-                                                   offset_x=-X_RANGE/2, offset_y=-Y_RANGE/2, 
-                                                   distribution_method="random")
-
-plot_voronoi(voronoi_data, seed_points, show_seed_points=True, show_voronoi=True)
-
-#distributions available:
-        #"halton"
-        #"fibonacci"
-        #"poisson"
-        #"random"
-
+# Run main function when script is executed
+if __name__ == "__main__":
+    main()
---
+++ voronoi_to_delauney.py
@@ -0,0 +1,103 @@
+from math import ceil, sqrt
+from numpy import zeros, empty
+
+# calculate the delauney triangulation of a dataset given the voronoi cells
+# as this 'should' be faster than recalculating the delauney set from scratch
+
+class quadrant:
+    def __init__(self, length=0, midpoint=None):
+        if length > 0:
+            self.length = length
+        else:
+            self.length = 0
+
+        if midpoint is None: 
+            self.midpoint = (0,0)
+        else:
+            self.midpoint = midpoint
+            
+        self.neighbours = []
+    
+
+class point:
+    def __init__(self):
+        non_boundary_edges = 0 
+        neighbours = []
+
+
+# divide grid into quandrants, each with some midpoint, to fill a bounding rectangle
+def generate_quadrants(voronoi_data):
+    min_x = 0
+    max_x = 0
+    min_y = 0
+    max_y = 0
+    quad_length = 0
+    quadrants = []    
+    
+    # find min and max x and y to determine bounding rectangle and use the shortest length for quadrant
+    for cells in voronoi_data:
+        for (x,y) in cells:
+            
+            if x < min_x:
+                min_x = x
+            elif x > max_x:
+                max_x = x
+            if y < min_y:
+                min_y = y
+            elif y > max_y:
+                max_y =y
+    
+    # use shortest side / sqrt of number of points to calculate length of quadrant squares (might need scaling)
+    quad_length = min([(max_x - min_x), (max_y - min_y)]) / round(sqrt(len(voronoi_data)))
+    num_rows = ceil((max_x - min_x)/quad_length) 
+    num_cols = ceil((max_y - min_y)/quad_length)
+    
+    print("quad length: ", quad_length)
+    print("num rows: ", num_rows)
+    print("num cols: ", num_cols)
+    
+    # create quadrants 
+    quadrants = empty((num_rows, num_cols), quadrant)
+    
+    for r in range(num_rows):
+        for c in range(num_cols):
+            q = quadrant(quad_length, 
+                         ((r * quad_length/2) + quad_length/2,
+                          (c * quad_length/2) + quad_length/2)
+                          )
+    
+            quadrants[r][c] = q
+            
+            # find all possible neighbouring quadrants, including diagonally adjacent
+            for i in range(r-1, r+2, 1):
+                for j in range(c-1, c+2, 1):
+                    # store valid neighbour rows and columns 
+                    if i >= 0 and i <= num_rows and j >= 0 and j <= num_cols:
+                        # excluding this quadrant
+                        if i == r and j == c:
+                            pass
+                        else:
+                            q.neighbours.append((i,j))
+    
+    return quadrants
+    
+    
+# loop over each point:
+    # loop over each quadrant
+        # if the distance from the point to the quadrant midpoint is less than size above
+            #  add point to that quadrant's list
+    
+            # calculate the number of neighbours of point:
+                # the corner points can be spotted easily as can edge pieces
+                # just calculate the number of non boundary edges
+
+# loop over each quadrant
+    # loop over each point inside quadrant
+            # loop over each edge (i.e. sets of vertices)
+                # while number of neighbours != number of non-boundary edges:
+                    # loop over each quadrant starting with closest to point 
+                    # (quadrants could be retrieved with a generator?)
+                        # loop over each point in that quandrant
+                            # loop over each edge in point:
+                                # if match then add to neighbours array and remaining matches -= 1
+                                    # also add the reverse match to the neighbours array of other point 
